4949b0e60ad673ddb419a491fe5bee889d2b9eb7
Nastya Pisanka <bonjozzy@gmail.com>
2023-12-20 20:18:58 +0300

    docs: extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,30 @@ module.exports = {
         "skyDark": "#979C9E",
         "green": {
           "template": "#BCFD49"
+        },
+        "template": {
+          "lightGray": "#CCC",
+          "ultraLightGray": "#F2F1F1",
+          "inkLight": "#6C7072",
+          "navi": "#7483BD",
+          "black": "#101010",
+          "gray": "#808080"
+        }
+      },
+      fill: {
+        "template": {
+          "black": "#000",
+          "gray": "#808080",
+          "white": "#fff",
+          "lightGray": "#F2F1F1"
+        }
+      },
+      stroke: {
+        "template": {
+          "black": "#000",
+          "gray": "#808080",
+          "white": "#fff",
+          "lightGray": "#F2F1F1"
         }
       },
       spacing: {
@@ -19,8 +43,26 @@ module.exports = {
       },
       width: {
         "300": "300%"
-      }
       },
+      fontFamily: {
+        body: ["Raleway", "sans-serif"],
+        title: ["Roboto", "sans-serif"],
+        accent: ["Inter", "sans-serif"]
+      },
+      lineHeight: {
+        "160": "160%"
+      },
+      gridTemplateColumns: {
+        "sliderArrows": "40px calc(100%-80px) 40px",
+        "recentCards": "repeat(3, 1fr)"
+      },
+      gridTemplateRows: {
+        "655px": "655px"
+      },
+      borderRadius: {
+        '6xl': '3rem'
+      }
+    },
   },
   plugins: [],
 
