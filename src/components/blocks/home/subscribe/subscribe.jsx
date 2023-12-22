f6adf4d581cf515d6a1454a783b51d98c514aa46
Nastya Pisanka <bonjozzy@gmail.com>
2023-12-22 21:13:26 +0300

    fix: fix subscribe section styles

--- src/components/blocks/home/subscribe/subscribe.jsx
+++ src/components/blocks/home/subscribe/subscribe.jsx
@@ -2,7 +2,7 @@ import SimpleInput from "../../../UI/inputs/simpleInput/simpleInput";
 
 function Subscribe(props){
   return (
-      <section className="subscribe">
+      <section className="subscribe mb-16">
         <div className="container flex justify-between items-center w-full">
           <h2 className="sub-title">Subscribe to our newsletter</h2>
           <SimpleInput placeholderSlot="Enter your Email" btnSlot="Subscribe"/>
