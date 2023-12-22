3adfa74166d6a792e38f1318749cd0b143e34a2f
Nastya Pisanka <bonjozzy@gmail.com>
2023-12-22 20:06:22 +0300

    refactor: clean code

--- src/components/UI/icons/icon/icon.jsx
+++ src/components/UI/icons/icon/icon.jsx
@@ -1,6 +1,5 @@
 import Icons from "./sprite.svg";
-function Icon({ id, width, height, isNeedToRotate, classNames }){
-  const additionalOptions = isNeedToRotate ? 'rotate-180' : 'rotate-0';
+function Icon({ id, classNames }){
   return (
       <svg className={classNames}>
         <use href={Icons + "#" + id}></use>
