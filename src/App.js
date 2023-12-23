d17a5b639ce35c23480fe4f2c1df7d02069e72e4
Nastya Pisanka <bonjozzy@gmail.com>
2023-12-23 13:50:28 +0300

    refactor: recent section

--- src/App.js
+++ src/App.js
@@ -99,6 +99,44 @@ const popularTripsData = [
   },
 ]
 
+const recentTripsData = [
+  {
+    "name": "barcelona",
+    "title": "Barcelona",
+    "minPrice": 2256,
+    "rate": 5,
+    "tripDuration": 11,
+    "placesCount": 1,
+    "countries": 1,
+    "description": "The one that gives you the best of Spanish culture, from beaches to palaces to tapas",
+    "imageSrc": "./assets/images/recently-viewed/barcelona.webp"
+  },
+  {
+    "name": "scandinavia",
+    "title": "Scandinavia",
+    "minPrice": 1900,
+    "rate": 5,
+    "tripDuration": 12,
+    "placesCount": 9,
+    "countries": 3,
+    "description": "From colourful Copenhagen to sophisticated Stockholm, this trip gives you 12 days of pure Scandinavian magic.",
+    "imageSrc": "./assets/images/recently-viewed/scandinavia.webp"
+  },
+  {
+    "name": "yellowstone",
+    "title": "Yellowstone and Grand Tetons",
+    "minPrice": 995,
+    "rate": 5,
+    "tripDuration": 1,
+    "placesCount": 9,
+    "countries": 1,
+    "description": "From the peaks of the Grand Tetons to Yellowstone, for 2 days of adventure in the world's oldest National Park",
+    "imageSrc": "./assets/images/recently-viewed/yellowstone.webp"
+  },
+]
+
+
+
 function App() {
   return (
       <div className="App">
@@ -107,7 +145,7 @@ function App() {
         <main>
           <Main mainTitle="DREAMY SUMMER HOLIDAYS" buttonTitle="See trips"/>
           <PopularSearches titlesList={filtersList}/>
-          <Recent />
+          <Recent recentTripsData={recentTripsData} sectionTitle="Recently viewed trips" bottomBtnText="View all trips" bottomBtnLink="/recent"/>
           <PopularTrips data={popularTripsData}/>
           <Why />
           <Reviews />
--- src/components/blocks/home/recent/recent.jsx
+++ src/components/blocks/home/recent/recent.jsx
@@ -2,49 +2,14 @@ import SectionTitle from "../../../UI/titles/section-title/section-title";
 import TripCard from "../../../UI/cards/trip-card/trip-card";
 import barcelona from "./barcelona.png";
 
-const incomingData = [
-  {
-    "name": "barcelona",
-    "title": "Barcelona",
-    "minPrice": 2256,
-    "rate": 5,
-    "tripDuration": 11,
-    "placesCount": 1,
-    "countries": 1,
-    "description": "The one that gives you the best of Spanish culture, from beaches to palaces to tapas",
-    "imageSrc": "./assets/images/recently-viewed/barcelona.webp"
-  },
-  {
-    "name": "scandinavia",
-    "title": "Scandinavia",
-    "minPrice": 1900,
-    "rate": 5,
-    "tripDuration": 12,
-    "placesCount": 9,
-    "countries": 3,
-    "description": "From colourful Copenhagen to sophisticated Stockholm, this trip gives you 12 days of pure Scandinavian magic.",
-    "imageSrc": "./assets/images/recently-viewed/scandinavia.webp"
-  },
-  {
-    "name": "yellowstone",
-    "title": "Yellowstone and Grand Tetons",
-    "minPrice": 995,
-    "rate": 5,
-    "tripDuration": 1,
-    "placesCount": 9,
-    "countries": 1,
-    "description": "From the peaks of the Grand Tetons to Yellowstone, for 2 days of adventure in the world's oldest National Park",
-    "imageSrc": "./assets/images/recently-viewed/yellowstone.webp"
-  },
-]
 
 
 
 
-function Recent(props){
+function Recent({ recentTripsData, sectionTitle, bottomBtnText, bottomBtnLink }){
   let key = -1;
 
-  const cards = incomingData.map(card => {
+  const cards = recentTripsData.map(card => {
     key += 1;
     return (
         <TripCard src={barcelona}
@@ -61,8 +26,8 @@ function Recent(props){
   return (
     <section className="recent">
       <div className="container relative pb-[129px] px-[40px]">
-        <SectionTitle title={"Recently viewed trips"} classNames="mb-16"/>
-        <a href="https://google.com" className="absolute top-0 right-0 link">View all trips</a>
+        <SectionTitle title={sectionTitle} classNames="mb-16"/>
+        <a href={bottomBtnLink} className="absolute top-0 right-0 link">{bottomBtnText}</a>
 
         <div className="cards-wrapper grid grid-cols-recentCards gap-8 w-full">
           {cards}
