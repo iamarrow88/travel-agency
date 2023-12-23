c8b76aaf2a1f2f5bab253c7e0d96b665f49b885b
Nastya Pisanka <bonjozzy@gmail.com>
2023-12-23 13:03:05 +0300

    fix: fix popular trips section styles

--- src/components/blocks/home/popular-trips/popular-trips.jsx
+++ src/components/blocks/home/popular-trips/popular-trips.jsx
@@ -29,7 +29,7 @@ const data = [
   },
 ]
 
-function PopularTrips(props){
+function PopularTrips(){
   let key = -1;
 
   const cards = data.map(tripsData => {
@@ -49,17 +49,17 @@ function PopularTrips(props){
     )
   })
   return (
-      <section className="most-popular pb-32">
-        <div className="container relative">
+      <section className="most-popular pt-16">
+        <div className="container most-popular__container relative pt-16 pb-32">
 
           <SectionTitle title="Most popular trips" classNames="mb-20"/>
 
-          <div className="buttons flex justify-end gap-2 absolute top-6 right-2">
+          <div className="buttons flex justify-end gap-2 absolute top-6 right-16">
              <ArrowButton id="arrow-right" svgClassNames="fill-white w-3 h-3 flex justify-center items-center" classNames="fill-white bg-template-ultraLightGray rounded-full w-8 h-8 rotate-180 flex justify-center items-center cursor-pointer" />
              <ArrowButton id="arrow-right" classNames="bg-template-ultraLightGray rounded-full w-8 h-8 flex justify-center items-center cursor-pointer" svgClassNames="fill-white w-3 h-3"/>
           </div>
 
-          <div className="cards-wrapper grid grid-cols-2 gap-8 mb-16">
+          <div className="cards-wrapper grid grid-cols-2 gap-8 mb-16 w-11/12 mx-auto">
             {cards}
           </div>
 
